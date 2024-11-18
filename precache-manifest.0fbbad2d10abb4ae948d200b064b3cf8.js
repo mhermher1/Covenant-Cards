276dcafc3f5f7465fcc0da778fb108112919fd65
Matt Herringshaw <mattherringshaw@MacBook-Air.lan>
2024-11-17 21:54:22 -0600

    Updates

---
+++ precache-manifest.0fbbad2d10abb4ae948d200b064b3cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f792f059cd5d1ea95ebff66c4b6d247a",
+    "url": "/Covenant-Cards/index.html"
+  },
+  {
+    "revision": "44c0be71c016cd9bc864",
+    "url": "/Covenant-Cards/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "c177093fd9383a590b21",
+    "url": "/Covenant-Cards/static/js/2.a873544b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Covenant-Cards/static/js/2.a873544b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3af9e076820595915e0",
+    "url": "/Covenant-Cards/static/js/3.a5527501.chunk.js"
+  },
+  {
+    "revision": "44c0be71c016cd9bc864",
+    "url": "/Covenant-Cards/static/js/main.bf4c8570.chunk.js"
+  },
+  {
+    "revision": "5139fca4097cf9e02445",
+    "url": "/Covenant-Cards/static/js/runtime-main.c73cb297.js"
+  }
+]);
